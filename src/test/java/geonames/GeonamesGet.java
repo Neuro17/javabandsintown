6114853297a4ff72f273a3e0a59d6eb2df4e0fa6
mastinux <andrea.pantaleo.93@gmail.com>
2014-12-12 12:37:50 +0100

    calculated TimeZone hours differences

--- src/test/java/geonames/GeonamesGet.java
+++ src/test/java/geonames/GeonamesGet.java
@@ -12,56 +12,56 @@ public class GeonamesGet extends GeonamesConnector{
 	public GeonamesGet(){
 		uriBld = new URIBuilder();
 		buildURI();
-		log.trace(uriBld);
+//		log.trace(uriBld);
 	}
 	
-	public GeonamesGet setLat(String lat) {
-		log.trace("Entering setLat");
-		uriBld.setParameter("lat", lat);
-		log.trace(super.uriBld);
-		log.trace("Exiting setLat");
+	public GeonamesGet setLat(double lat) {
+//		log.trace("Entering setLat");
+		uriBld.setParameter("lat", Double.toString(lat));
+//		log.trace(super.uriBld);
+//		log.trace("Exiting setLat");
 		return this;
 	}
 	
-	public GeonamesGet setLng(String lng) {
-		log.trace("Entering setLng");
-		uriBld.setParameter("lng", lng);
-		log.trace(super.uriBld);
-		log.trace("Exiting setLng");
+	public GeonamesGet setLng(double lng) {
+//		log.trace("Entering setLng");
+		uriBld.setParameter("lng", Double.toString(lng));
+//		log.trace(super.uriBld);
+//		log.trace("Exiting setLng");
 		return this;
 	}
 	
 
 	public GeonamesGet setUsername() {
-		log.trace("Entering setUsername");
+//		log.trace("Entering setUsername");
 		uriBld.setParameter("username",GeonamesConfig.getUsername());
-		log.trace(super.uriBld);
-		log.trace("Exiting setUsername");
+//		log.trace(super.uriBld);
+//		log.trace("Exiting setUsername");
 		return this;
 	}
 	
 	public GeonamesGet setJson(){
-		log.trace("Entering setJson");
+//		log.trace("Entering setJson");
 		uriBld.setPath(GeonamesConfig.getTimeZoneJson() + "?");
-		log.trace("Exiting setJson");
+//		log.trace("Exiting setJson");
 		return this;
 	}
-	
+	/*
+	 * restituisce il timezoneId della lat e lng specificate
+	 */
 	public String search(){
-		log.trace("Entering search on geonames.org");
+//		log.trace("Entering search on geonames.org");
 		JsonObject geoNamesAsJson;
-		String s = "";
+		String s;
 		
 		build();
-		log.debug(uri);
+//		log.debug(uri);
 		geoNamesAsJson = executeRequest(uri);
-		log.debug(geoNamesAsJson);
+//		log.debug(geoNamesAsJson);
 		
-		s += "\ncountryNAme: " +geoNamesAsJson.getAsJsonObject().get("countryName").getAsString();
-		s += "\ntimezoneId: " + geoNamesAsJson.getAsJsonObject().get("timezoneId").getAsString();
-		s += "\nlat: " + geoNamesAsJson.getAsJsonObject().get("lat").getAsDouble();
-		s += "\nlng: " + geoNamesAsJson.getAsJsonObject().get("lng").getAsDouble();
-		log.trace("Exiting search from geonames.org");
+		s = geoNamesAsJson.getAsJsonObject().get("timezoneId").getAsString();
+		
+//		log.trace("Exiting search from geonames.org");
 		
 		return s;
 	}
--- src/test/java/geonames/TimezoneTest.java
+++ src/test/java/geonames/TimezoneTest.java
@@ -1,7 +1,22 @@
 package geonames;
 
+import java.util.ArrayList;
+import java.util.TimeZone;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import search.Bandsintown;
+
+import entity.Event;
+
+
 
 public class TimezoneTest {
+	
+	private final static Logger log = LogManager.getLogger(TimezoneTest.class);
+
+	
 	public static void main(String args[]){
 //		System.out.println("Siamo nell'" + TimeZone.getDefault().getDisplayName());
 //		System.out.println("milli secondi da aggiungere all'orario attuale per raggiungere il GMT: "+TimeZone.getDefault().getDSTSavings());
@@ -11,11 +26,42 @@ public class TimezoneTest {
 //		System.out.println(s);
 //		System.out.println("Ore da aggiungere a GMT per avere l'ora US/Hawaii: " +TimeZone.getTimeZone("US/Hawaii").getRawOffset()/3600000);
 //		System.out.println("La nostra ora rispetta quella legale: " +TimeZone.getDefault().observesDaylightTime());
-		GeonamesGet gng = new GeonamesGet();
 
-//		test validità url
+		GeonamesGet gng = new GeonamesGet();
+//		ArrayList<Event> events = new ArrayList<Event>();
+		Bandsintown bandsintown = new Bandsintown();
+		ArrayList<Event> events = null;
+//test validità url
 //		System.out.println(gng.setLat("47.01").setLng("10.2").setUsername());
+
+//imposto parametri di ricerca per particolare lat e lng
+		/* latitudine distanza dall equatore 
+		 * longitudine distanza dal meridiano */
+//		System.out.println("Time zone id trovato: " + gng.setLat("51.4791").setLng("0").setUsername().search());
 		
-		System.out.println(gng.setLat("47.01").setLng("10.2").setUsername().search());
+//		events = bandsintown.getEvents.setArtist("empire empire").asJson().setDate("all").search();
+//		for(Event e : events)
+//			log.debug("city: " + e.getVenue().getCity() + "" +
+//					" - lat: " + e.getVenue().getLatitude() + "" +
+//							" - lng: " + e.getVenue().getLongitude() +"" +
+//									" - hour: " + e.getDatetime().getHourOfDay() + "" +
+//											" - nimutes: " + e.getDatetime().getMinuteOfHour());
+
+//imposto fuso orario di riferimento quello di londra
+		TimeZone.getTimeZone("Europe/London");
+//ottengo timezoneId per ogni citta degli eventi
+		events = bandsintown.getEvents.setArtist("david guetta").asJson().setDate("upcoming").search();
+//		for(Event e : events)
+//			log.debug(gng	.setLat(e.getVenue().getLatitude())
+//							.setLng(e.getVenue().getLongitude())
+//							.setUsername()
+//							.search());
+//calcolo ore da aggiungere alla TimeZone di default
+		for(Event e : events)
+			log.trace("Ore da aggiungere alla TimeZone 'Europe/London'" +
+					"" +TimeZone.getTimeZone(gng	.setLat(e.getVenue().getLatitude())
+													.setLng(e.getVenue().getLongitude())
+													.setUsername()
+													.search()).getRawOffset()/3600000);
 	}
 }
